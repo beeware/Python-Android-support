b064e0f4d355d4f264bc7a23d4e2b62f7b099638
Asheesh Laroia <asheesh@asheesh.org>
2020-01-10 15:58:11 -0800

    Copy .so files by contents, dereferencing symlinks

--- 3.7.sh
+++ 3.7.sh
@@ -5,4 +5,5 @@ IMAGE_NAME="python-android-support-3.7:${TAG_FOR_THIS_BUILD}"
 docker build -t "$IMAGE_NAME" -f 3.7.Dockerfile .
 rm -rf ./output/3.7
 mkdir -p output/3.7
-docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint rsync "$IMAGE_NAME" -a --delete /opt/python-build/approot/. /mnt/.
+# Using rsync -L so that libpython3.7.so.1.0.0 is copied into a file, not as a symlink.
+docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint rsync "$IMAGE_NAME" -aL --delete /opt/python-build/approot/. /mnt/.
