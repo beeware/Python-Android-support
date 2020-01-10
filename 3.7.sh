dbc8e91a96adb1cd0a1fef92b700ebb19520804c
Asheesh Laroia <asheesh@asheesh.org>
2020-01-10 09:47:36 -0800

    Export generated libs & assets from the container

--- 3.7.sh
+++ 3.7.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -eou pipefail
-docker build -f 3.7.Dockerfile .
-
+TAG_FOR_THIS_BUILD="local_$(python3 -c 'import time; print(int(time.time() * 1e9))')"
+IMAGE_NAME="python-android-support-3.7:${TAG_FOR_THIS_BUILD}"
+docker build -t "$IMAGE_NAME" -f 3.7.Dockerfile .
+rm -rf ./output/3.7
+mkdir -p output/3.7/
+docker run -v $PWD/output/3.7/:/mnt/output --rm --entrypoint cp "$IMAGE_NAME" -a applibs/ /mnt/output
+docker run -v $PWD/output/3.7/:/mnt/output --rm --entrypoint cp "$IMAGE_NAME" -a assets/ /mnt/output
