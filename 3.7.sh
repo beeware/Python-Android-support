45824c3e2d36640ff64d152e91ad7edddffd6f71
Asheesh Laroia <asheesh@asheesh.org>
2020-01-29 10:07:28 -0800

    Save build output to a ZIP file
    In addition, refactor shell script:
    
    - Explicitly check for dependencies at start.
    - Avoid creating tagged images. This makes cleaning up the Docker context easier.
    - Make code comments shorter & clearer.

--- 3.7.sh
+++ 3.7.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
+
+# Set bash strict mode.
 set -eou pipefail
-chmod -R u+rw output/3.7 || sudo chown -R "$USER" output/3.7
-rm -rf ./output/3.7
-mkdir -p output/3.7
-for TARGET_ABI_SHORTNAME in x86 x86_64 armeabi-v7a arm64-v8a; do
-    # Using ANDROID_API_LEVEL=21 allows us to have a localeconv function (admittedly a non-working one),
-    # which makes compiling (well, linking) Python easier.
+
+# Check dependencies.
+function require() {
+    local FOUND="no"
+    which "$1" > /dev/null && FOUND="yes"
+    if [ "$FOUND" = "no" ] ; then
+	echo "Missing dependency: $1
+
+Please install it. One of following might work, depending on your system:
+
+$ sudo apt-get install $1
+$ brew install $1
+
+Exiting."
+	exit 1
+    fi
+}
+
+for dependency in docker python3 zip; do
+    require "$dependency"
+done
+
+# Extract image ID from `docker build` output. Used by `build_one_abi`.
+IMAGE_NAME_TEMPFILE="$(mktemp)"
+function extract_image_name() {
+    tee >(tail -n1 | sed -e 's,Successfully built \([^ ]*\),\1,' > "$IMAGE_NAME_TEMPFILE")
+}
+
+function build_one_abi() {
+    TARGET_ABI_SHORTNAME="$1"
+    # Using ANDROID_API_LEVEL=21 for two reasons:
     #
-    # Additionally, 64-bit architectures only start existing at API level 21. If we want to decrease this
-    # to 19, be sure to special-case 64-bit architectures to use 21.
+    # - >= 21 gives us a `localeconv` libc function (admittedly a
+    #   non-working one), which makes compiling (well, linking) Python
+    #   easier.
+    #
+    # - 64-bit architectures only start existing at API level 21.
     ANDROID_API_LEVEL=21
-    # Compute the compiler name & binutils prefix name. Quoting https://developer.android.com/ndk/guides/other_build_systems :
-    # "Note: For 32-bit ARM, the compiler is prefixed with armv7a-linux-androideabi, but the binutils tools are prefixed with arm-linux-androideabi. For other architectures, the prefixes are the same for all tools."
+    # Compute the compiler name & binutils prefix name. See also:
+    # https://developer.android.com/ndk/guides/other_build_systems
+
     case "${TARGET_ABI_SHORTNAME}" in
     armeabi-v7a)
         TOOLCHAIN_TRIPLE="arm-linux-androideabi"
@@ -30,7 +61,9 @@ for TARGET_ABI_SHORTNAME in x86 x86_64 armeabi-v7a arm64-v8a; do
         COMPILER_TRIPLE="${TOOLCHAIN_TRIPLE}${ANDROID_API_LEVEL}"
         ;;
     esac
-    # Compute the OpenSSL build target name.
+
+    # Compute OpenSSL build target name. We avoid using OpenSSL's built-in
+    # Android support because it does not seem to give us any benefits.
     case "${TARGET_ABI_SHORTNAME}" in
     armeabi-v7a)
         OPENSSL_BUILD_TARGET="linux-generic32"
@@ -45,10 +78,37 @@ for TARGET_ABI_SHORTNAME in x86 x86_64 armeabi-v7a arm64-v8a; do
         OPENSSL_BUILD_TARGET="linux-x86_64"
         ;;
     esac
-    TAG_FOR_THIS_BUILD="local_$(python3 -c 'import time; print(int(time.time() * 1e9))')"
-    IMAGE_NAME="python-android-support-3.7:${TAG_FOR_THIS_BUILD}"
-    docker build --build-arg COMPILER_TRIPLE="${COMPILER_TRIPLE}" --build-arg OPENSSL_BUILD_TARGET="$OPENSSL_BUILD_TARGET" --build-arg TARGET_ABI_SHORTNAME="$TARGET_ABI_SHORTNAME" --build-arg TOOLCHAIN_TRIPLE="$TOOLCHAIN_TRIPLE" --build-arg ANDROID_API_LEVEL="$ANDROID_API_LEVEL" -t "$IMAGE_NAME" -f 3.7.Dockerfile .
+
+    docker build --build-arg COMPILER_TRIPLE="${COMPILER_TRIPLE}" --build-arg OPENSSL_BUILD_TARGET="$OPENSSL_BUILD_TARGET" --build-arg TARGET_ABI_SHORTNAME="$TARGET_ABI_SHORTNAME" --build-arg TOOLCHAIN_TRIPLE="$TOOLCHAIN_TRIPLE" --build-arg ANDROID_API_LEVEL="$ANDROID_API_LEVEL" -f 3.7.Dockerfile . | extract_image_name
+    local IMAGE_NAME
+    IMAGE_NAME="$(cat $IMAGE_NAME_TEMPFILE)"
+    if [ -z "$IMAGE_NAME" ] ; then
+	echo 'Unable to find image name. Did Docker build succeed? Aborting.'
+	exit 1
+    fi
+
     # Using rsync -L so that libpython3.7.so.1.0.0 is copied into a file, not as a symlink.
-    docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint rsync "$IMAGE_NAME" -aL /opt/python-build/approot/. /mnt/.
+    docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint rsync "$IMAGE_NAME" -aL  /opt/python-build/approot/. /mnt/.
     docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint rsync "$IMAGE_NAME" -aL /opt/python-build/built/python/include/python3.7m/pyconfig.h /mnt/
-done
+}
+
+function main() {
+    # Clear the output directory.
+    rm -rf ./output/3.7
+    mkdir -p output/3.7
+
+    for TARGET_ABI_SHORTNAME in x86 x86_64 armeabi-v7a arm64-v8a; do
+	build_one_abi "$TARGET_ABI_SHORTNAME"
+    done
+
+    # When using Docker on Linux, the `rsync` command creates files owned by root.
+    # Compute the user ID and group ID of this script on the non-Docker side, and ask
+    # Docker to adjust permissions accordingly.
+    USER_AND_GROUP="$(id -u):$(id -g)"
+    docker run -v $PWD/output/3.7/:/mnt/ --rm --entrypoint chown -R "$USER_AND_GROUP" .
+
+    # Make a ZIP file.
+    cd output/3.7 && zip -q -i 'app/*' -0 -r ../3.7.zip . && cd ../..
+}
+
+main
