5e36964e148a5b8ee7edaff9a05f8c1c88dbee43
Russell Keith-Magee <russell@keith-magee.com>
2020-06-21 08:47:03 +0800

    Fix compiler symbols and x86 header name.

--- patches/all/pyconfig.h
+++ patches/all/pyconfig.h
@@ -1,15 +1,15 @@
+#ifdef __aarch64__
+#include "pyconfig-arm64-v8a.h"
+#endif
+
 #ifdef __arm__
 #include "pyconfig-armeabi-v7a.h"
 #endif
 
-#ifdef __arm64__
-#include "pyconfig-arm64-v8a.h"
+#ifdef __x86_64__
+#include "pyconfig-x86_64.h"
 #endif
 
 #ifdef __i386__
-#include "pyconfig-x386.h"
-#endif
-
-#ifdef __x86_64__
-#include "pyconfig-x86_64.h"
+#include "pyconfig-x86.h"
 #endif
